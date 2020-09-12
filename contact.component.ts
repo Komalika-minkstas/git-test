d6812aac5172b2f1a3ce2283085a8f427eceab15
Komalika-minkstas <69082937+Komalika-minkstas@users.noreply.github.com>
2020-09-13 00:49:07 +0530

    Reactive Forms Part-1

--- contact.component.ts
+++ contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Feedback, ContactType } from '../shared/feedback';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+ }
 
   ngOnInit() {
+  }
+  
+  createForm() {
+    this.feedbackForm = this.fb.group({
+     firstname: '',
+     lastname: '',
+     telnum: 0,
+     email: '',
+     agree: false,
+     contacttype: 'None',
+     message: ''
+  });
+}
+
+  onSubmit() {
+    this.feeback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset();
   }
 
 }
