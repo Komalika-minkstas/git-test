9783fa59a1051e7b86f5524289889763a55de3ef
Komalika-minkstas <69082937+Komalika-minkstas@users.noreply.github.com>
2020-10-07 13:59:55 +0530

    Error Messages

--- dishdetail.component.ts
+++ dishdetail.component.ts
@@ -21,7 +21,7 @@ export class DishdetailComponent implements OnInit {
   dishIds: string[];
   prev: string;
   next: string;
-  
+  errMess: string;
   
   
   
@@ -40,7 +40,8 @@ export class DishdetailComponent implements OnInit {
   
    this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
+         errmess => this.errMess = <any>errmess);
   }
 
   setPrevNext(dishId: string) {
